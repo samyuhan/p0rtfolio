fbe9e95b382d182102a1c7080ef4ee88cdabc20d
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-14 19:35:51 -0700

    add homepage

---
+++ app/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Homepage from "./components/Homepage";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import About from "./components/About";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/Contact";
+// import Resume from "./components/Resume";
+
+function App() {
+  // const [currentPage, setCurrentPage] = useState('About Me');
+
+  // // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  // const renderPage = () => {
+  //   if (currentPage === 'About Me') {
+  //     return <About />;
+  //   }
+  //   if (currentPage === 'Portfolio') {
+  //     return <Portfolio />;
+  //   }
+  //   if (currentPage === 'Contact') {
+  //     return <Contact />;
+  //   }
+  //   if (currentPage === 'Resume') {
+  //     return <Resume />;
+  //   }
+  // };
+
+  // const handlePageChange = (page) => setCurrentPage(page);
+
+  // return (
+  //   <div>
+  //     <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+  //     <Header />
+  //     {renderPage()}
+  //     <Footer />
+  //   </div>
+  // );
+
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
+}
+
+export default App;
---
+++ app/src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Homepage.css';
+
+const styles = {
+  front: {
+    background: "#242526"
+    
+  }
+};
+
+function Homepage() {
+  return (
+    <div style={styles.front} className="front">
+      <p className="title">SAMANTHA YUHAN</p>
+      <p className="desc">coder.&nbsp; creator.&nbsp; aspiring designer.&nbsp;</p> 
+    </div>
+  );
+}
+
+export default Homepage;
