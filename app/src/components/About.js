5cd0ff08f2b288250ab1656255d87dc139797c93
Samantha Yuhan <sysweettooth1@gmail.com>
2021-09-23 22:41:56 -0700

    add styling to about me page

--- app/src/components/About.js
+++ app/src/components/About.js
@@ -3,10 +3,14 @@ import '../styles/About.css';
 
 function About(){
     return (
-        <div>
-            <h1>About Me</h1>
-            <p>Sam currently works from home in Irvine, California.</p>
-            <p>She likes to </p>
+        <div className="abt">
+            {/* <img src="snow.png" alt="Picture of me snowboarding" height="200" className="mepic"></img> */}
+            
+            <p className="abtme">Hello, I'm <a href="#" className="abtmehover">Sam<img src="snow.jpg" width="400px"/></a>! I just graduated from <a href="#" className="abtmehover">UC&nbsp;Berkeley<img src="berkeley.jpg" width="400px"/></a> with a degree in Data Science.
+            With my tech and art background, I want to find creative and effective solutions to 
+            I'm currently working from home in <a href="#" className="abtmehover">Irvine,&nbsp;CA<img src="irvine.jpg" width="400px"/></a>.
+            Feel free to browse around or <a href="mailto:syuhan@berkeley.edu" className="abtmehover">contact</a> me if you have any questions!</p>
+            <p className="background">ABOUT ME</p>
         </div>
     );
 }
